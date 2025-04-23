05f27ed8a52597e7b59e95d7f80f5a81440384ad
Noah Mattos Oudejans <noahmattosoudejans@icloud.com>
2025-04-23 10:21:41 +0200

    Finished ex06

---
+++ cpp/01/ex06/Harl.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmattos- <nmattos-@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/16 14:30:53 by nmattos-          #+#    #+#             */
+/*   Updated: 2025/04/16 14:30:55 by nmattos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class Harl
+{
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+	public:
+		Harl() {};
+		~Harl() {};
+
+		void	complain(std::string level);
+};
+
+#define DEBUG "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
+#define INFO "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!"
+#define WARNING "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month."
+#define ERROR "This is unacceptable! I want to speak to the manager now."
